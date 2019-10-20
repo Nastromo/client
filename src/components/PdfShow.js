eba0c9fc7d269bf52f9efe62e7e03c429f3dc460
nastromo <nastromo@Solaris.local>
2019-10-20 18:32:19 +0300

    done

--- src/components/PdfShow.js
+++ src/components/PdfShow.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 
+
+
 export class PdfShow extends Component {
     render() {
         let params = (new URL(window.location.href)).searchParams;
         let title = params.get('title');
         let port = 8050;
+        let host = `192.168.20.216`;
         if (window.location.href.includes(`localhost`)) {
             port = 9000;
+            host = `localhost`;
         }
         return (
             <div>
-                <embed src={`http://localhost:${port}/${title}`} width="100%" height="1000px" />
-
-
+                <embed src={`http://${host}:${port}/${title}`} width="100%" height="1000px" />
             </div>
-
         )
     }
 }
